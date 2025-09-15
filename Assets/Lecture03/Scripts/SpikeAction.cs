b9ae18b8889e4228d4cdcc32d936c9b7eb3daee4
윤대호 <yundaeho@yundaehoui-MacBookAir.local>
2025-09-15 16:48:34 +0900

    9월 15일

---
+++ Assets/Lecture03/Scripts/SpikeAction.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SpikeAction : MonoBehaviour
+{
+    float start_point;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("SpikeDestroyer"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
